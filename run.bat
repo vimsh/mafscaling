1ffd72f15d207218584df5e39fcff056ba6be426
vimsh <vladimsh@yahoo.com>
2015-01-29 20:28:21 -0500

    updated notes and set windows format

--- run.bat
+++ run.bat
@@ -1,27 +1,52 @@
 @echo off
-REM *** Adjust path to javaw.exe as required for your Java system installation ***
-REM set PATH=%PATH%;C:\ProgramData\Oracle\Java\javapath
+
+REM *** Set/Change path to javaw.exe if you have multiple Java installations or if has not been added to the PATH ***
+REM set PATH=C:\Program Files\Java\jre17u3\bin;%PATH%
+
 REM
-REM *** Windows system ***
+
+REM *** Windows system skin ***
+
 start javaw -Dswing.defaultlaf=com.sun.java.swing.plaf.windows.WindowsLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM
-REM *** New Java Swing ***
+
+REM *** Nimbus - New Java Swing . REQUIRES JAVA 7 OR GREATER ***
+
 REM start javaw -Dswing.defaultlaf=com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM
+
 REM *** Linux Motif ***
+
 REM start javaw -Dswing.defaultlaf=com.sun.java.swing.plaf.motif.MotifLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM
+
 REM *** Variations of skinned default Java Swing look and feel ***
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.aero.AeroLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.smart.SmartLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.mint.MintLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.fast.FastLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.luna.LunaLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.texture.TextureLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.mcwin.McWinLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.aluminium.AluminiumLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.acryl.AcrylLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.graphite.GraphiteLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.hifi.HiFiLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.noire.NoireLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar
+
 REM start javaw -Dswing.defaultlaf=com.jtattoo.plaf.bernstein.BernsteinLookAndFeel -Xms64M -Xmx512M -jar MafScaling.jar

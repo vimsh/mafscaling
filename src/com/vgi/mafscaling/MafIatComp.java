048f112cc464723ecf646d5416aa4dcd0536f5a0
vimsh <vladimsh@yahoo.com>
2015-06-29 09:49:28 -0400

    revisited logic for time parsing

--- src/com/vgi/mafscaling/MafIatComp.java
+++ src/com/vgi/mafscaling/MafIatComp.java
@@ -298,7 +298,6 @@ public class MafIatComp extends ACompCalc {
 	                int row = getLogTableEmptyRow();
 	                long time = 0;
 	                long prevTime = 0;
-	                long tmbase = 0;
 	                double thrtlMaxChange2 = thrtlMaxChange * 2.0;
 	                double throttle = 0;
 	                double pThrottle = 0;
@@ -335,12 +334,9 @@ public class MafIatComp extends ACompCalc {
 		                    	throttle = Double.valueOf(flds[logThrottleAngleColIdx]);
 	                        	// Calculate dV/dt
                             	prevTime = time;
-                            	time = Utils.parseTime(flds[logTimeColIdx], tmbase);
-                            	if (tmbase == 0) {
-                            		tmbase = time;
-                            		if (time > 1000)
-                            			time = 0;
-                            	}
+                            	if (prevTime == 0)
+                            		Utils.resetBaseTime(flds[logTimeColIdx]);
+                            	time = Utils.parseTime(flds[logTimeColIdx]);
                             	pmafv = mafv;
                             	mafv = Double.valueOf(flds[logMafvColIdx]);
                             	if ((time - prevTime) == 0.0)

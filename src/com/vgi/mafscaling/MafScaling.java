642920bc6b6cca1219e83072161ddee3acd0d9c9
vimsh <vladimsh@yahoo.com>
2015-08-08 13:27:03 -0400

    bumped version number

--- src/com/vgi/mafscaling/MafScaling.java
+++ src/com/vgi/mafscaling/MafScaling.java
@@ -35,7 +35,7 @@ import org.apache.log4j.Logger;
 
 public class MafScaling {
     private static final Logger logger = Logger.getLogger(MafScaling.class);
-    private static final String Title = "MAF Scaling - v2.1.1";
+    private static final String Title = "MAF Scaling - v2.1.2";
     private static final String OLTabName = "<html>Open Loop</html>";
     private static final String CLTabName = "<html>Closed Loop</html>";
     private static final String RTabName = "<html>Rescale</html>";
